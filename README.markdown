1d7716178fc73bcca45687fc40d9f47f01d6a81c
Joshua Newman <j@orson.com>
2011-07-14 20:34:01 -0400

    Initial commit, 3.0

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# Ruby on Rails Tutorial: first application
+
+This is Joshua's first application in Ruby. WOOT!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = 'cfb99e67a1d27d7c3c0d11a32a2023682933ff763b9d2606a4327d2ddc81e247c6a63b2561e4b90204815e5de691f68ab467f9ba4f78d84a3a6374bf21114bdb'
